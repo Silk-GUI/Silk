a18cf34c1aac0726fa5df2c772dafe4450f84ca7
zodern <fakeemail@fakeembail.com>
2014-11-04 09:19:58 -0600

    changed loading animation

--- client/js/load.js
+++ client/js/load.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
-  setTimeout(function(){$(".loader").css("display", "none");},3000) 
+$(document).ready(function () {
+
+  $(".loader span").css("animationIterationCount", "1");
+  window.setTimeout(function () {
+    $(".loader").fadeOut();
+  }, 3000)
+
 });
