40f1b85fce405331103a3a47ea50fce9d40a08a0
formula1 <samtobia@gmail.com>
2014-11-08 17:06:24 -0800

    made the file explorer work

--- client/fileExplorer/js/fileExplorer.js
+++ client/fileExplorer/js/fileExplorer.js
@@ -1,25 +1,66 @@
 var path = "/";
-var files = [];
-var folders = [];
 
-function seperateFiles(list){
-  for(var i = 0; i < list.length; ++i){
-    var item = list[i];
-    var index = item.indexOf(".");
-    if(index == -1){
-      folders.push(item);
-      
-    }
-    else{
-      files.push(item);
-    }
-    console.log(files);
-    console.log(folders);
-  }
+function file_explorer(elem){
+  if(typeof elem == "undefined")
+    elem = "body";
+  this.elem = jQuery(elem);
+  this.files = this.elem.find(".files");
+  this.cd = this.elem.find(".current_path");
+  var that = this;
+  jQuery(this.elem).on("click",".current_path a,.files .directory a", function(e){
+    e.preventDefault();
+    that.changeDirectory($(this).attr("href"))
+    return false;
+  })
+  jQuery(this.elem).on("click",".files .file a", function(e){
+    e.preventDefault();
+    alert("You're going to have to setup a default view for mimetype: "+$(this).parent().attr("data-mime"));
+    return false;
+  })
+  this.changeDirectory("/");
 }
 
-// get files in / directory
-methods.call("fe/list/home", {}, function (err, list) {
- seperateFiles(list);
-  document.write(list);
-});
+file_explorer.prototype.changeDirectory = function(href){
+  // get files in / directory
+  var that = this;
+  methods.call("fe/list/path", [href], function (err, list) {
+    if(err) return alert(err);
+    that.processList(href,list);
+  });
+}
+
+file_explorer.prototype.processCD = function(href){
+  aref = href.split("/");
+  if(/\/$/.test(href)){
+    aref.pop();
+  }
+  this.cd.empty();
+  var netref = "";
+  for(var i=0;i<aref.length;i++){
+    var name = aref[i];
+    if(name == ""){
+      name = "root";
+    }else
+      netref += "/"+name
+    this.cd.append("/<a href='"+netref+"'>"+name+"</a>");
+  }
+}
+file_explorer.prototype.processList = function(href, list){
+this.processCD(href);
+this.files.empty();
+for(var i=0;i<list.length;i++){
+  var item = list[i];
+  var el = jQuery("<li><a href='"+item.path+"'>"+item.name+"</a></li>");
+  this.files.append(el);
+  if(item.isDir){
+    el.addClass("directory");
+  }else{
+    el.addClass("file");
+    el.attr("data-mime", item.mime);
+  }
+}
+}
+
+jQuery(function($){
+  new file_explorer();
+})
--- fileExplorer.js
+++ fileExplorer.js
@@ -1,16 +1,27 @@
 var fs = require('fs');
-
+var mime = require("mime")
 
 
 
 methods.add({
   "fe/list/path": function (path) {
-    fs.readdir(path, function (err, files) {
-      console.log(files);
-    });
-  },
-  "fe/list/home": function () {
-    var files = fs.readdirSync("/");
-    return files
+    if(typeof path == "undefined")
+      path = "/";
+    else
+      path = path[0];
+    if(!/\/$/.test(path))
+      path += "/";
+    var files = fs.readdirSync(path);
+    for(var i=0;i<files.length;i++){
+      files[i] = {
+        name: files[i],
+        path:path+files[i],
+      }
+      var stats = fs.statSync(files[i].path);
+      files[i].isDir = stats.isDirectory();
+      if(!files[i].isDir)
+        files[i].mime = mime.lookup(files[i].path);
+    }
+    return files;
   }
 })
