64eefba6185fec9a87d3dd4c5324e49c68c364c0
zodern <zodern@users.noreply.github.com>
2014-12-10 12:06:46 -0600

    Added defaults page to set up.

--- core/setup/setup.js
+++ core/setup/setup.js
@@ -1,10 +1,16 @@
 // lets user choose window manager, then installs wm and apps requested by wm
-module.exports = function(app){
+module.exports = function (app) {
   app.get('/', function (req, res) {
-  res.sendFile(__root + "/core/setup/public/index.html");
-});
-  app.get('/choose.html', function(req, res){
+    res.sendFile(__root + "/core/setup/public/index.html");
+  });
+  app.get('/choose.html', function (req, res) {
     res.sendFile(__root + "/core/setup/public/choose.html");
+  });
+  app.get('/css/setup.css', function (req, res) {
+    res.sendFile(__root + "/core/setup/public/css/setup.css");
+  });
+  app.get('/defaults.html', function (req, res) {
+    res.sendFile(__root + "/core/setup/public/defaults.html");
   });
 
 }
