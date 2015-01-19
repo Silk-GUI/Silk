905caafdec7128068e35da758ac5ac56026c0e5c
zodern <zodern@users.noreply.github.com>
2015-01-19 12:33:23 -0600

    remote supports multiple ports.

--- core/remote.js
+++ core/remote.js
@@ -1,41 +1,107 @@
- // make app availalbe outisde nodeos
+var domain = require('domain'),
+  ports = {},
+  autoStart = false;
+/**
+ * Manages localtunnel for one port.
+ * @constructor
+ * @param {number} port - port number
+ */
+function Remote(port) {
+  this.port = port;
+  this.status = 'stopped';
+  this.url = null;
+  var remoteDomain = domain.create();
+  var that = this;
+  this.close = function () {};
 
- start = function (options) {
-   var remoteDomain = require('domain').create();
+  remoteDomain.on('error', function (err) {
+    console.log(err);
+    try {
+      that.close();
+    } catch (e) {
+      console.log(e);
+    }
+    that.status = 'stopped';
+    that.start();
+  });
 
-   remoteDomain.on('error', function (err) {
-     console.log(err);
-     try {
-       Silk.get("remote/close")();
-     } catch (e) {
-       console.log(e);
-     }
-   })
+  this.start = function () {
+    var that = this;
+    remoteDomain.run(function () {
+      var localtunnel = require('localtunnel');
+      //TODO if status is stopped and url is not null try to start with the url.
+      localtunnel(that.port, function (err, tunnel) {
+        if (err) {
+          console.log(err);
+          this.url = null;
+          this.status = 'stopped';
+          return;
+        }
+        this.close = tunnel.close;
+        this.url = tunnel.url;
+        this.status = 'running';
+        Silk.set("remote/url", tunnel.url);
+        Silk.set("remote/close", tunnel.close);
 
-   remoteDomain.add(Silk);
+        console.log("Go to " + tunnel.url + " to remotely access Silk");
 
-   remoteDomain.run(function () {
-     var localtunnel = require('localtunnel');
-     localtunnel(3000, function (err, tunnel) {
-       if (err) {
-         console.log(err);
-         return;
-       }
+        tunnel.on("error", function (err) {
+          console.log(err);
 
-       Silk.set("remote/url", tunnel.url);
-       Silk.set("remote/close", tunnel.close);
+        });
+        tunnel.on("close", function () {
+          console.log("remote closed");
+        });
+      });
+    })
+  }
 
-       console.log("Go to " + tunnel.url + " to remotely access Silk");
+  if (autoStart === true) {
+    this.start();
+  }
+}
 
-       tunnel.on("error", function (err) {
-         console.log(err);
+ports[3000] = new Remote(3000);
 
-       });
-       tunnel.on("close", function () {
-         console.log("remote closed");
-       })
-     });
+/**
+ * Makes app availalbe outisde this computer.  Starts localtunnel.
+ * {number} options - localtunnel options.  Not used yet
+ */
+var start = function (port) {
+  if (typeof port === 'number') {
+    ports[port].start();
+  } else {
+    if (typeof port === 'boolean') {
+      autoStart = true;
+    }
+    for (port in ports) {
+      ports[port].start();
+    }
+  }
+}
 
-   })
- }
- Silk.set('remote/start', start);
+var close = function (port) {
+  if (typeof port === 'number') {
+    ports[port].close();
+  } else {
+    if (typeof port === 'boolean') {
+      autoStart = false;
+    }
+    for (port in ports) {
+      ports[port].close();
+    }
+  }
+}
+
+function addPort(port) {
+  ports[port] = new Remote(port);
+}
+
+function removePort(port){
+  ports[port].close();
+  delete ports[port];
+}
+Silk.set('remote/start', start);
+Silk.set('remote/close', close);
+Silk.set('remote/ports', ports);
+Silk.set('remote/addPort', addPort);
