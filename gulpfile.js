46c2ed4b0ab8f694535b2cc186bfd5c908ac25c9
zodern <zodern@users.noreply.github.com>
2015-03-23 10:51:59 -0500

    Will use gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var browserify = require('gulp-browatchify');
+var reactify = require('reactify');
+
+gulp.tas('browserify', function () {
+	gulp.src('./')
+})
+
+gulp.task('default', function () {
+
+});
