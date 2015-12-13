9356a89fee911c8f3ea23f996941db347fbcd56b
zodern <zodern@users.noreply.github.com>
2015-12-12 21:09:43 -0600

    reformat main.js

--- main.js
+++ main.js
@@ -12,13 +12,13 @@ process.title = "Silk GUI";
 var WatchData = require('./core/watch_data.js');
 global.Silk = new WatchData();
 
-var app = express(),
-  server,
-  wss,
-  windows,
-  url,
-  toLoad = 2,
-  loaded = 0;
+var app    = express(),
+    server,
+    wss,
+    windows,
+    url,
+    toLoad = 2,
+    loaded = 0;
 
 program
   .version('0.4.2')
@@ -33,78 +33,78 @@ if(process.argv[2] === 'help') {
 
 logger.logLevel = program.dev ? 0 : 1;
 
-if(program.dev === true){
+if(program.dev === true) {
   global.debug = console.log;
 } else {
-  global.debug = function () {};
+  global.debug = function () {
+  };
 }
 
 
-
-
-function start () {
-    var spinner = new logger.Spinner('Starting Silk');
-    spinner.start();
-    // hides spinner and shows url when finished loading;
-    function loader() {
-      loaded += 1;
-      if (loaded === toLoad) {
-        spinner.stop();
-        process.stdout.write('\r ' + url);
-        console.log('');
-      }
+function start() {
+  var spinner = new logger.Spinner('Starting Silk');
+  spinner.start();
+  // hides spinner and shows url when finished loading;
+  function loader() {
+    loaded += 1;
+    if(loaded === toLoad) {
+      spinner.stop();
+      process.stdout.write('\r ' + url);
+      console.log('');
     }
-    // app.get('/', function (req, res) {
-    //     res.sendFile(__root + "/window-manager/public/index.html");
-    // });
+  }
+
+  // app.get('/', function (req, res) {
+  //     res.sendFile(__root + "/window-manager/public/index.html");
+  // });
 
 // static files for client
-    //app.use(express.static(__dirname + '/window-manager/public'));
-    app.get(/^\/bc\//, require(__root + "/core/bower_static.js"));
-    app.get("/api.js", require(__root + "/core/client_api.js"));
+  //app.use(express.static(__dirname + '/window-manager/public'));
+  app.get(/^\/bc\//, require(__root + "/core/bower_static.js"));
+  app.get("/api.js", require(__root + "/core/client_api.js"));
 
-    server = app.listen(3000, function () {
-        var address = server.address();
-        // IPv6 addresses start with ::ffff:.  Is there any
-        // problems with removing it?
-        url = 'Silk at http://localhost:' + address.port;
-        loader();
-    });
+  server = app.listen(3000, function () {
+    var address = server.address();
+    // IPv6 addresses start with ::ffff:.  Is there any
+    // problems with removing it?
+    url = 'Silk at http://localhost:' + address.port;
+    loader();
+  });
 
-    var sockOptions = {
-        sockjs_url: '//cdn.jsdelivr.net/sockjs/0.3.4/sockjs.min.js'
+  var sockOptions = {
+    sockjs_url: '//cdn.jsdelivr.net/sockjs/0.3.4/sockjs.min.js'
+  };
+
+  if(!program.dev) {
+    sockOptions.log = function (severity, message) {
+      if(severity === 'error') {
+        console.log(message);
+      }
     };
+  }
 
-    if (!program.dev) {
-        sockOptions.log = function (severity, message) {
-            if (severity === 'error') {
-                console.log(message);
-            }
-        };
-    }
+  wss = SockJS.createServer(sockOptions);
+  wss.installHandlers(server, {
+    prefix: '/ws'
+  });
 
-    wss = SockJS.createServer(sockOptions);
-    wss.installHandlers(server, {
-        prefix: '/ws'
-    });
+  var forkFramework = require(__root + "/core/fork_framework");
+  forkFramework(app, wss, function () {
+    loader();
+  });
 
-   var forkFramework = require(__root + "/core/fork_framework");
-   forkFramework(app, wss, function () {
-        loader();
-    });
+  forkFramework.startWindowManager(configJson.windowManager, app, function (e, d) {
+    console.log('started app', e, d);
+  });
 
-    forkFramework.startWindowManager(configJson.windowManager, app, function(e, d){
-       console.log('started app', e, d);
-    });
+  require('./core/remote.js');
 
-    require('./core/remote.js');
+  if(program.remote) {
+    Silk.get('remote/start')(true);
+  }
 
-    if (program.remote) {
-        Silk.get('remote/start')(true);
-    }
-
-    if (program.open) {
-        require('./core/nw/open.js')(program.devtools);
-    }
+  if(program.open) {
+    require('./core/nw/open.js')(program.devtools);
+  }
 }
 start();
