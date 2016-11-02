f732c80921a0d5e042b963651ee39130c7e0f6ee
zodern <zodern@users.noreply.github.com>
2016-11-01 21:12:19 -0500

    Fix removing app from cwd

--- core/commands/remove_app.js
+++ core/commands/remove_app.js
@@ -3,6 +3,7 @@ var db = require('../db.js');
 var externalApps = db.collections.appId;
 
 function removeApp(path) {
+  path = path || process.cwd();
   console.log('removing app ' + path);
   externalApps.remove({ path: path }, function () {
     console.log('finished');
